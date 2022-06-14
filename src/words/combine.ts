825953eb25b9ba51e2c77e62485c6a892790f1df
George MacKerron <george@mackerron.co.uk>
2022-06-14 13:10:59 +0100

    Fixed word list

--- src/words/combine.ts
+++ src/words/combine.ts
@@ -4,11 +4,15 @@ import * as fs from 'fs';
 const
   wikiWordCounts = JSON.parse(fs.readFileSync('wordCounts.json', { encoding: 'utf8' })),
   allWikiWords = Object.keys(wikiWordCounts),
-  wikiWords = allWikiWords.filter(x => wikiWordCounts[x] >= 20),
+  wikiWords = allWikiWords.filter(x => wikiWordCounts[x] >= 20);
 
+wikiWordCounts.constructor = wikiWordCounts.constructors;  // suspicious gap, otherwise filled by constructor function
+
+const
   web2 = fs.readFileSync('web2.txt', { encoding: 'utf8' }).split(/[\r\n]+/),
   aspell = fs.readFileSync('aspell_en.txt', { encoding: 'utf8' }).split(/[\r\n]+/)
-    .filter(w => w.length > 3),
+    .filter(w => w.length > 2),
+
   // from http://crr.ugent.be/archives/2045
   wordPrevs = Object.fromEntries(fs.readFileSync('wordprevalences.csv', { encoding: 'utf8' }).split(/[\r\n]+/)
     .map(line => {
@@ -20,18 +24,43 @@ const
   allWords = [...prevWords, ...wikiWords, ...web2, ...aspell]
     .map(s => s.toLowerCase().replace(/[^a-z]+/g, ''))
     .filter(w => w.length > 1 || w === 'a' || w === 'i' || w === 'o'),
-  result: Record<string, number> = {},
 
-  weight = (word: string) => parseFloat((
-    1 +
-    (wordPrevs[word] ? 15 * Math.pow(wordPrevs[word], 2) : 0) +  // .5 words are already pretty obscure, so we square this measure
-    (!wordPrevs[word] && word.endsWith('s') && wordPrevs[word.slice(0, -1)] ? 15 * Math.pow(wordPrevs[word.slice(0, -1)], 2) : 0) +
-    (!wordPrevs[word] && word.endsWith('ies') && wordPrevs[word.slice(0, -3) + 'y'] ? 15 * Math.pow(wordPrevs[word.slice(0, -3) + 'y'], 2) : 0) +
-    (wikiWordCounts[word] ? Math.log10(Math.max(10, wikiWordCounts[word])) : 0)
-  ).toFixed(2));
+  weight = (word: string) => {
+    let weight = 1;
+
+    const prev = wordPrevs[word];
+    if (prev) {
+      // console.log(1, prev);
+      weight += 10 * prev * prev;  // .5 prevalence words are already pretty obscure, so we square the measure
+
+    } else if (word.endsWith('ies')) {
+      const
+        iesy = word.slice(0, -3) + 'y',
+        iesyPrev = wordPrevs[iesy];
+
+      // console.log(2, iesyPrev);
+      if (iesyPrev) weight += 9 * iesyPrev * iesyPrev;
+
+    } else if (word.endsWith('s')) {
+      const
+        sless = word.slice(0, -1),
+        slessPrev = wordPrevs[sless];
+
+      // console.log(3, slessPrev);
+      if (slessPrev) weight += 9 * slessPrev * slessPrev;
+    }
+
+    const wikiwc = wikiWordCounts[word];
+    // console.log(4, wikiwc);
+    if (wikiwc) weight += Math.log10(Math.max(10, wikiwc));
+
+    weight = parseFloat(weight.toFixed(2));
+    return weight;
+  },
+
+  result: Record<string, number> = Object.create(null);
 
 allWords.sort((a, b) => weight(b) - weight(a));
-
 for (const word of allWords) result[word] = weight(word);
 
 fs.writeFileSync('../../dist/words.json', JSON.stringify(result, null, 2));
