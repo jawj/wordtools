9a644340f8a22fe13e6763b446a5c12ca99d1894
George MacKerron <george@mackerron.co.uk>
2025-02-05 13:46:34 +0000

    progress

--- src/anag.ts
+++ src/anag.ts
@@ -1,15 +1,20 @@
 import { getWordWeights } from './words';
 
+
 let wordsByWordable: Record<string, string[]>;
-async function makeHash() {
+function makeHash(wordWeights: Awaited<ReturnType<typeof getWordWeights>>) {
   if (wordsByWordable) return;
   wordsByWordable = {};
-  for (const word in await getWordWeights()) {
+  for (const word in wordWeights) {
     const wordable = word.split('').sort().join('');
     wordsByWordable[wordable] ??= [];
     wordsByWordable[wordable].push(word);
   }
 }
+
+const wordWeights = await getWordWeights();
+makeHash(wordWeights);
+
 
 type LetterCount = [string[], number[]];
 function countLetters(s: string) {
@@ -50,27 +55,21 @@ function createAllocation(maxValues: number[]) {
   return mb;
 }
 
-function nextAllocation(mb: Uint32Array, debug = false) {
-  const len = mb.length - 2; // exclude count and index at last 2 positions
-  const comboTotal = mb[len];
-  const comboIndex = ++mb[len + 1];
+function nextAllocation(alloc: Uint32Array) {
+  const len = alloc.length - 2;  // exclude count and index at last 2 positions
+  const halfway = alloc[len];
+  const allocIndex = ++alloc[len + 1];
   for (let i = 0; i < len; i++) {
-    const col = mb[i];
+    const col = alloc[i];
     const max = col >>> 16;
-    const v = (col & 0xffff) + 1; // increment value
-    // if (debug && i === len - 1) console.log('---');
+    const v = (col & 0xffff) + 1;  // increment value
     if (v <= max) {
-      mb[i] = (max << 16) | v;
-      // if (debug) {
-      //   if (comboIndex === comboTotal) console.log('===');
-      //   return true;
-      // }
-      return comboIndex < comboTotal;
+      alloc[i] = (max << 16) | v;
+      return allocIndex < halfway;
     }
-    mb[i] = max << 16;  // v is implicitly zero 
+    alloc[i] = max << 16;  // v is implicitly zero 
   }
-  // if (debug) return false;
-  throw new Error('If we get here, we will have seen every combination twice');
+  throw new Error('This should be impossible');
 }
 
 function* splitsFromString(s: string) {
@@ -100,6 +99,39 @@ function* wordablesFromString(s: string, kept: string[] = []): Generator<string[
   }
 }
 
+function* combinations(s: string[][]) {
+  const lengths = s.map(s => s.length);
+  const indices = s.map(() => 0);
+  while (true) {
+    yield s.map((s, i) => s[indices[i]]);
+    for (let i = 0; i < indices.length; i++) {
+      indices[i]++;
+      if (indices[i] < lengths[i]) break;
+      if (i === indices.length - 1) return;
+      indices[i] = 0;
+    }
+  }
+}
 
-await makeHash();
-for (const wordables of wordablesFromString('elvises')) console.log(wordables.map(w => wordsByWordable[w].join('/')));
+function goodness(words: string[]) {
+  // start with weight of lowest-weighed word, after adjusting weight to give a slight preference for longer words
+  const min = words.reduce((memo, word) => {
+    const adjustedWeight = wordWeights[word] + .1 * word.length;
+    return memo < adjustedWeight ? memo : adjustedWeight;
+  }, Infinity);
+  // adjust with a strong preference for fewer words in total
+  return min / Math.pow(words.length, 3);
+}
+
+//for (const wordables of wordablesFromString('elvises')) console.log(wordables.map(w => wordsByWordable[w].join('/')));
+
+const anags = [];
+for (const wordables of wordablesFromString('louispotter')) {
+  const words = wordables.map(w => wordsByWordable[w]);
+  for (const anag of combinations(words)) anags.push([anag, goodness(anag)] as const);
+  // for (const anag of combinations(words)) {
+  //   console.log(anag, goodness(anag));
+  // }
+}
+anags.sort(([, a], [, b]) => b - a);
+for (const [anag, score] of anags.slice(0, 25)) console.log(anag.join(' '));
