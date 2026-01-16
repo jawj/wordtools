01270049477a9eb5c5514901450b544e503fda90
George MacKerron <george@mackerron.co.uk>
2026-01-16 17:13:39 +0000

    Auto-start anagrams if letters provided

--- src/Anagram.ts
+++ src/Anagram.ts
@@ -5,6 +5,7 @@ import { credits, waitingMessage } from './common';
 import { stringWithCommas } from './util';
 import type { AnagramStatus, Anagram } from './anag';
 import * as comlink from 'comlink';
+import { standardise } from './words';
 
 interface AnagramAttrs {
   letters: string;
@@ -25,8 +26,6 @@ function groupByLength(anagrams: Anagram[], max = 10) {
 }
 
 export function Anagram(vnode: m.Vnode<AnagramAttrs>) {
-  let loading = true;
-  let dictionarySize = 0;
   let groupByWordCount = false;
 
   let status: AnagramStatus = {
@@ -36,17 +35,23 @@ export function Anagram(vnode: m.Vnode<AnagramAttrs>) {
   };
 
   function start() {
-    status.working = true;
-    const letters = (document.querySelector('#anag-input') as HTMLInputElement).value;
-    void wrappedWorker.find(letters, keepN, reportEveryN, comlink.proxy(async newStatus => {
-      const prevStatus = status;
-      status = newStatus;
-      m.redraw();
-      return !prevStatus.working;
-    }));
+    const letters = standardise((document.querySelector('#anag-input') as HTMLInputElement).value);
+    if (letters) {
+      status.working = true;
+      void wrappedWorker.find(letters, keepN, reportEveryN, comlink.proxy(async newStatus => {
+        const prevStatus = status;
+        status = newStatus;
+        m.redraw();
+        return !prevStatus.working;
+      }));
+    }
   }
 
   return {
+    oncreate(vnode: m.Vnode<AnagramAttrs>) {
+      const letters = standardise(vnode.attrs.letters);
+      if (letters && letters !== emptyValue) start();
+    },
     view(vnode: m.Vnode<AnagramAttrs>) {
       let letters = vnode.attrs.letters === emptyValue ? '' : vnode.attrs.letters;
 
@@ -54,76 +59,70 @@ export function Anagram(vnode: m.Vnode<AnagramAttrs>) {
         m(Nav, { page: 'anagram' }),
         m('.pattern',
           m('h2', 'Find anagrams'),
-          loading ? m('.message', waitingMessage('Loading dictionary')) :
-            m('.interface',
-              m('.pattern-entry',
-                m('input#anag-input', {
-                  type: 'search',
-                  autocorrect: 'off',
-                  autocomplete: 'off',
-                  autocapitalize: 'none',
-                  spellcheck: false,
-                  size: 15,
-                  value: letters,
-                  disabled: status.working,
-                  onchange(e: { currentTarget: HTMLInputElement; }) {
-                    const { value } = e.currentTarget;
-                    m.route.set(
-                      '/anagram/:letters',
-                      { ...vnode.attrs, letters: value === '' ? '-' : value },
-                    );
-                  },
-                  onkeyup(e: any) {
-                    if (e.key === 'Enter') {
-                      if (status.working) status.working = false;
-                      else start()
-                    }
-                    e.redraw = false;
-                  }
-                }),
-                status.working ? [
-                  // Stop button
-                  m('button', {
-                    onclick() {
-                      status.working = false;
-                    }
-                  }, 'Stop'),
-                  m('.progress', waitingMessage(
-                    `${stringWithCommas(status.evaluated)} evaluated` +
-                    (status.evaluated > keepN ? `, ${stringWithCommas(keepN)} kept` : '')
-                  )),
-                ] : [
-                  // Find button
-                  m('button', { onclick: start }, 'Find'),
-                  status.evaluated !== 0 && m('.progress',
-                    `${stringWithCommas(status.evaluated)} found` +
-                    (status.evaluated > keepN ? `, ${stringWithCommas(keepN)} kept` : '')
-                  ),
-                ]
-              ),
-              m('#grouping', m('label', m('input[type=checkbox]', {
-                checked: groupByWordCount,
+          m('.interface',
+            m('.pattern-entry',
+              m('input#anag-input', {
+                type: 'search',
+                autocorrect: 'off',
+                autocomplete: 'off',
+                autocapitalize: 'none',
+                spellcheck: false,
+                size: 15,
+                value: letters,
+                disabled: status.working,
                 onchange(e: { currentTarget: HTMLInputElement; }) {
-                  groupByWordCount = e.currentTarget.checked;
+                  const { value } = e.currentTarget;
+                  m.route.set(
+                    '/anagram/:letters',
+                    { ...vnode.attrs, letters: value === '' ? '-' : value },
+                  );
+                },
+                onkeyup(e: any) {
+                  if (e.key === 'Enter') {
+                    if (status.working) status.working = false;
+                    else start()
+                  }
+                  e.redraw = false;
                 }
-              }), ' Group by word count')),
-              m('.matches',
-                groupByWordCount ?
-                  groupByLength(status.anagrams).map((group, wordCount) => group.length > 0 && [
-                    m('h3', `${wordCount} words`),
-                    group.map(a => m('span.match', a[0].join(' ')))
-                  ]) :
-                  status.anagrams.map(a => m('span.match', a[0].join(' ')))
-              ),
+              }),
+              status.working ? [
+                // Stop button
+                m('button', {
+                  onclick() {
+                    status.working = false;
+                  }
+                }, 'Stop'),
+                m('.progress', waitingMessage(
+                  `${stringWithCommas(status.evaluated)} evaluated` +
+                  (status.evaluated > keepN ? `, ${stringWithCommas(keepN)} kept` : '')
+                )),
+              ] : [
+                // Find button
+                m('button', { onclick: start }, 'Find'),
+                status.evaluated !== 0 && m('.progress',
+                  `${stringWithCommas(status.evaluated)} found` +
+                  (status.evaluated > keepN ? `, ${stringWithCommas(keepN)} kept` : '')
+                ),
+              ]
             ),
+            m('#grouping', m('label', m('input[type=checkbox]', {
+              checked: groupByWordCount,
+              onchange(e: { currentTarget: HTMLInputElement; }) {
+                groupByWordCount = e.currentTarget.checked;
+              }
+            }), ' Group by word count')),
+            m('.matches',
+              groupByWordCount ?
+                groupByLength(status.anagrams).map((group, wordCount) => group.length > 0 && [
+                  m('h3', `${wordCount} words`),
+                  group.map(a => m('span.match', a[0].join(' ')))
+                ]) :
+                status.anagrams.map(a => m('span.match', a[0].join(' ')))
+            ),
+          ),
           credits(' Anagrams are retained and ranked by preferring those where the least common word is more common, those with fewer words, and (only very slightly) those where the mean word is more common.'),
         )
       );
-    },
-    async oninit() {
-      dictionarySize = await wrappedWorker.getWordsCount();
-      loading = false;
-      m.redraw();
     },
     onremove() {
       status.working = false;
